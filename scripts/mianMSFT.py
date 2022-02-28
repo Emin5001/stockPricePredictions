d6e72f44ed4ff731b380f5bc3ece4aec0f1be87c
Emin Koroglu <65010715+Emin5001@users.noreply.github.com>
2022-02-28 13:25:01 -0600

    completed msft stock prediction

--- scripts/mianMSFT.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-msft_stock_data = pd.read_csv('/Users/eminn/PycharmProjects/stockPricePredictions/data/MSFT.csv')
-print(msft_stock_data.head())
